4fc57c7e74b9c04935b17cb51a2fa79e430f8746
GuilleGarzon <guillegarzon.gg@gmail.com>
2023-04-21 15:54:23 -0500

    feat: Add JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,12 @@
 import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>      
-    </>
+    <JournalLayout>
+
+      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, esse earum nostrum repellendus modi aperiam? Ad fugiat pariatur voluptatibus, laboriosam temporibus, repudiandae corporis quis maiores at quae veritatis nobis officiis?</Typography>      
+    
+    </JournalLayout>
   )
 }
