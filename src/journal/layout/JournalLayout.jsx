0de0c3f27a8a6124a7a19986951b66a5cae29570
GuilleGarzon <guillegarzon.gg@gmail.com>
2023-04-21 17:08:06 -0500

    feat: Add Toolbar in JournalLayout

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Toolbar } from '@mui/material';
 import { NavBar, SideBar } from '../components';
 
 const drawerWidth = 280;
@@ -15,7 +15,7 @@ export const JournalLayout = ({ children }) => {
         sx={{ flexGrow: 1, p: 3 }}
       >
 
-        {/* Toolbar */}
+        <Toolbar />
 
         { children }
 
