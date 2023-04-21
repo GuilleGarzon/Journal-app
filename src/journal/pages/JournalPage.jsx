ab84dfb864595ea9f3425db48910983d0f2b83f1
GuilleGarzon <guillegarzon.gg@gmail.com>
2023-04-21 17:04:01 -0500

    feat: Add NothingSelected View

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,13 @@
-import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
 
-      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, esse earum nostrum repellendus modi aperiam? Ad fugiat pariatur voluptatibus, laboriosam temporibus, repudiandae corporis quis maiores at quae veritatis nobis officiis?</Typography>      
+      {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, esse earum nostrum repellendus modi aperiam? Ad fugiat pariatur voluptatibus, laboriosam temporibus, repudiandae corporis quis maiores at quae veritatis nobis officiis?</Typography> */}
+
+      <NothingSelectedView />
     
     </JournalLayout>
   )
