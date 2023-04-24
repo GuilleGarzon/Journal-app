09c1e2eb5c8ae06727a8ee6653542875abf8d118
GuilleGarzon <guillegarzon.gg@gmail.com>
2023-04-24 17:00:29 -0500

    feat: Add NoteView component

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,5 @@
 import { JournalLayout } from '../layout/JournalLayout';
-import { NothingSelectedView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
@@ -7,7 +7,8 @@ export const JournalPage = () => {
 
       {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, esse earum nostrum repellendus modi aperiam? Ad fugiat pariatur voluptatibus, laboriosam temporibus, repudiandae corporis quis maiores at quae veritatis nobis officiis?</Typography> */}
 
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     
     </JournalLayout>
   )
